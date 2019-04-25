ca07a0fa04f01df733a5d699ad0d83770b663c42
AymenFJA <aymenfadhel@yahoo.com>
2019-04-25 18:18:53 -0400

    Tiling Method is here :)

--- mainSift.cpp
+++ mainSift.cpp
@@ -15,7 +15,7 @@
 #include "cudaImage.h"
 #include "cudaSift.h"
 #include <vector>
-
+#include <stdlib.h>  // for strtol
 int ImproveHomography(SiftData &data, float *homography, int numLoops, float minScore, float maxAmbiguity, float thresh);
 void PrintMatchData(SiftData &siftData1, SiftData &siftData2, CudaImage &img);
 void MatchAll(SiftData &siftData1, SiftData &siftData2, float *homography);
@@ -28,16 +28,16 @@ double ScaleUp(CudaImage &res, CudaImage &src);
 int main(int argc, char **argv) 
 {    
   int devNum = 0, imgSet = 0;
-  if( argc != 3)
+  if( argc != 11)
     {
      std::cout <<" Usage: display_image ImageToLoadAndDisplay" << std::endl;
      return -1;
     }
-  cv::Mat limg, rimg;
-  cv::imread(argv[1], 0).convertTo(limg, CV_32FC1);
-  cv::imread(argv[2], 0).convertTo(rimg, CV_32FC1);
+  cv::Mat src_limg, trg_rimg;
+  cv::imread(argv[1], 0).convertTo(src_limg, CV_32FC1);
+  cv::imread(argv[6], 0).convertTo(trg_rimg, CV_32FC1);
   //check for valid GEOTIFF input 
-  if(! limg.data || ! rimg.data)
+  if(! src_limg.data || ! trg_rimg.data)
     {
         std::cout <<  "Could not open or find the image" << std::endl ;
         return -1;
@@ -46,9 +46,20 @@ int main(int argc, char **argv)
      {
 
   std::cout << "GEOTIFF image1 detected  " <<argv[1]<<std::endl;
-  std::cout << "GEOTIFF image2 detected  " <<argv[2]<<std::endl;
+  std::cout << "GEOTIFF image2 detected  " <<argv[6]<<std::endl;
 
      }
+  //Tiling Method @aymenalsaadi :) 
+  int src_x1= strtol(argv[2],NULL, 10); 
+  int src_y1= strtol(argv[3],NULL, 10);
+  int src_x2= strtol(argv[4],NULL, 10);
+  int src_y2= strtol(argv[5],NULL, 10);
+  int trg_x1= strtol(argv[7],NULL, 10);
+  int trg_y1= strtol(argv[8],NULL, 10);
+  int trg_x2= strtol(argv[9],NULL, 10);
+  int trg_y2= strtol(argv[10],NULL,10);
+  cv::Mat limg = cv::Mat(src_limg, cv::Rect(src_x1,src_y1,src_x2,src_y2));
+  cv::Mat rimg = cv::Mat(trg_rimg, cv::Rect(trg_x1,trg_y1,trg_x2,trg_y2)); 
   
   unsigned int w = limg.cols;
   unsigned int h = limg.rows;
