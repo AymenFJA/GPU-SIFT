b103cba67388ae77bc81b2de13c3000fa33ee5ee
AymenFJA <aymenfadhel@yahoo.com>
2020-04-08 00:45:44 -0400

    cleanup

--- mps/mps_sift_gpu.sh
+++ mps/mps_sift_gpu.sh
@@ -4,7 +4,9 @@
 export CUDA_VISIBLE_DEVICES=0
 lrank=$OMPI_COMM_WORLD_LOCAL_RANK
 
-case ${lrank} in[0])
+case ${lrank} in
+
+[0])
     export CUDA_MPS_PIPE_DIRECTORY=/home/aymen/MPS/mps_0
     ../cudasift /home/aymen/imagery/datasets/20160903222711_10300.tif 0 0 5000 5000 /aymen/imagery/datasets/20160903222711_10311.tif 0 0 5000 5000
     ;;
@@ -12,6 +14,6 @@ case ${lrank} in[0])
     export CUDA_MPS_PIPE_DIRECTORY=/home/aymen/MPS/mps_1
     ../cudasift /home/aymen/imagery/datasets/20160903222711_10320.tif 0 0 5000 5000 /aymen/imagery/datasets/20160903222711_10321.tif 0 0 5000 5000
     ;;
-esac
 
+esac
 echo "done"
--- mps/start_mps_server.sh
+++ mps/start_mps_server.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#SBATCH -N 2
-#SBATCH -p GPU
-#SBATCH -t 5:00:00
-#SBATCH --gres=gpu:p100:2
-
 
 num_gpus=2
 
@@ -15,15 +10,20 @@ do
 mkdir /home/aymen/MPS/mps_$i  #MPS Deamon folder
 mkdir /home/aymen/MPS/mps_log_$i #MPS Log folder
 
-export CUDA_VISIBLE_DEVICES=$i #Make one of the GPUs visable per iteration
+#For some reason MPS was not detecting any devices 
+#So we had to set of the UUIDs
+UUIDs=$(nvidia-smi -L)	
+echo $UUIDs
+
+export CUDA_VISIBLE_DEVICES= UUIDs #Make one of the GPUs visable per iteration
 
 export CUDA_MPS_PIPE_DIRECTORY=/home/aymen/MPS/mps_$i # Set the path of the MPS Deamon folder 
 export CUDA_MPS_LOG_DIRECTORY=/home/aymen/MPS/mps_log_$i # Set the path of the MPS logs folder 
 
-nvidia-smi -i 2 -c EXCLUSIVE_PROCESS # It is not necessary. But it is a good idea to ensure all your CUDA processes access the GPU via MPS, 
-                                     # rather than having some accidentally connecting to the GPU directly, or running multiple MPS servers by mistake.
+nvidia-smi -i 2 -c EXCLUSIVE_PROCESS 
+
 
 # Start the CUDA-MPS server
-srun --gres=gpu:2 nvidia-cuda-mps-control -d&
+nvidia-cuda-mps-control -d&
 
-end do
+done
